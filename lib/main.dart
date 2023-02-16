60b5c2f92fc2cb912429b36f2601ea5f315f9985
Olusegun Adesina <shegmatech@gmail.com>
2023-02-16 17:38:23 +0100

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
-import 'package:elibrary/views/book_detail_page.dart';
 import 'package:flutter/material.dart';
 import 'package:elibrary/views/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main(){
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
